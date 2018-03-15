e66de0279a54a1be1678cc251acffbbbbdebb24f
Egor <tka4enkoegor@gmail.com>
2018-03-15 10:34:47 +0200

    Set default group for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,16 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors, uniq: true
-  belongs_to :group
+  belongs_to :group, required: true
   validates :title, presence: true, length: { maximum: 255 }
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  before_save { Group.any? ? self.group_id = Group.first : Group.create!(name: 'General') }
+  before_validation { set_default_group }
+
+  private
+
+  def set_default_group
+    Group.any? ? self.group_id = Group.first.id : Group.create!(name: 'General')
+  end
 end
